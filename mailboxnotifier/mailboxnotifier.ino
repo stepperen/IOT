66d647908423b436a133fd0c27f0ea9490d8a4dd
David <david-step@hotmail.com>
2018-11-20 18:25:03 +0100

    Edit file

--- mailboxnotifier/mailboxnotifier.ino
+++ mailboxnotifier/mailboxnotifier.ino
@@ -5,14 +5,12 @@ int flagGotmail = 90;
   
 
 void setup() {
-
   flagServo.attach(D0);
   flagServo.write(flagStart);
   Particle.publish("twilio_sms", "Mailbox Notifier is ready!", PRIVATE);
 }
 
-//LOOP
-void loop() {
+void loop(){
   delay(60000); // 60 sec
   System.sleep(A4, FALLING); //Put to sleep, wake up if contact moves
   flagServo.write(flagGotmail); // Set flag
